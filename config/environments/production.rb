5de56b05e8a6bbc2f78ea03bf918c53aa7d8e1bf
Kevin Chang <changkaimin.big@gmail.com>
2015-03-30 09:19:09 +0800

    use mailgun add-on on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Mailer
+  config.action_mailer.default_url_options = { :host => 'wordsfarm.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: ENV['MAILGUN_SMTP_SERVER'],
+    port: ENV['MAILGUN_SMTP_PORT'],
+    user_name: ENV['MAILGUN_SMTP_LOGIN'],
+    password: ENV['MAILGUN_SMTP_PASSWORD'],
+    domain: 'wordsfarm.herokuapp.com',
+    authentication: :plain,
+  }
 end
