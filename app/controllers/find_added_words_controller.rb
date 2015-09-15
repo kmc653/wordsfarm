10c1faf8050c93c3fb5cbafffe7b68cccb2eefd6
Kevin Chang <changkaimin.big@gmail.com>
2015-09-16 00:29:06 +0800

    add find_added_words_controller_spec

--- app/controllers/find_added_words_controller.rb
+++ app/controllers/find_added_words_controller.rb
@@ -3,8 +3,9 @@ class FindAddedWordsController < ApplicationController
     if params[:word]
       if Vocabulary.exists?(word: params[:word])
         @word = Vocabulary.find_by word: params[:word]
+        flash[:success] = "Found Successfully!"
       else
-        flash[:error] = "Couldn't find '#{@word}'. Please try another one."
+        flash[:error] = "Couldn't find '#{params[:word]}'. Please try another one."
       end
     end
   end
